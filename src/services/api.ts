5763cdbb43788b83a425f1efee50edd69b4b802e
Bruno Kenji <bruno.momose@gmail.com>
2021-07-24 23:28:51 -0300

    Atualizado o link de api fake do My-JSON-Server no api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/produtos'
+    baseURL: 'https://my-json-server.typicode.com/bmomose/hc2-moo-moo-project/produtos'
 })
 
 export default api
