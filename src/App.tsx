1635cd90fff89596265b121cb456b6398894ecf6
Bruno Kenji <bruno.momose@gmail.com>
2021-07-24 22:40:31 -0300

    Adicionando variável de ambiente

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Home from './view/Home';
 // import { Container } from './styles';
 
 const App: React.FC = () => {
+  fetch(process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1')
+    .then(() => console.log('DEU CERTO!!!'))
+    .catch(() => console.log('DEU RUIM...'))
   return <Home />;
 }
 
